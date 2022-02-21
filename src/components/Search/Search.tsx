ff091e047c578d66bbfc1f480384429c92e12af7
mdbeauche <michael.beauchemin@gmail.com>
2022-02-21 13:42:39 -0800

    api connection

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,4 +1,9 @@
+import { useState } from 'react';
+import axios, { AxiosResponse } from 'axios';
+
 const Search = () => {
+  const [searchResults, setSearchResults] = useState<Array<string>>([]);
+
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
     // submit search
@@ -8,19 +13,39 @@ const Search = () => {
 
     const search = target.search.value;
 
+    interface SearchResult {
+      title: string;
+      photo: string;
+      shortDescription: string;
+      description: string;
+    }
+
+    interface SearchResultResponse {
+      data: SearchResult[];
+    }
+
+    axios.post<SearchResultResponse>('http://localhost:5000/search', { term: search }).then((response) => {
+      console.log('Server response:');
+      console.log(response.data);
+    });
+
     console.log('search: ', search);
+    setSearchResults([search]);
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Search:
-          <input type="text" name="search" defaultValue="" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Search:
+            <input type="text" name="search" defaultValue="" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+      <div>Results: {searchResults}</div>
+    </>
   );
 };
 
